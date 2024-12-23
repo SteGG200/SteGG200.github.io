96df593f5c79a11cc05f3987e073ec5f1c536ec0
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-23 23:38:04 +0700

    Create header

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import NavBar from "./NavBar.svelte";
+
+
+</script>
+
+<header class="fixed top-0 left-0 w-full border-b-1 border-yellow">
+	<div class="w-2/3 max-xl:w-11/12 mx-auto text-yellow flex justify-between py-6">
+		<a class="font-bold text-2xl" href="/">SteGG</a>
+		<NavBar/>
+	</div>
+</header>
---
+++ src/components/header/NavBar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<nav class="text-lg font-semibold space-x-10">
+	<a href="/">Information</a>
+	<a href="/">Education</a>
+	<a href="/">Projects</a>
+	<a href="/">Contact</a>
+	<a href="/more">More</a>
+</nav>
