21e157a72269032281e86786b95eeac86ffac97d
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-24 18:02:16 +0700

    Design Header and Introduction Section for responsive device

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -6,10 +6,10 @@
 
 <header class="sticky w-full border-b-1 border-yellow bg-dark z-10">
 	<div class="w-full text-yellow flex py-6">
-		<div class="w-1/2 flex justify-center">
+		<div class="w-1/2 max-2xl:w-1/3 flex justify-center">
 			<a class="font-bold text-2xl" href="/">SteGG</a>
 		</div>
-		<div class="w-1/2 flex justify-center">
+		<div class="max-lg:hidden w-1/2 max-2xl:w-2/3 flex justify-center">
 			<NavBar/>
 		</div>
 	</div>
--- src/components/sections/IntroSection.svelte
+++ src/components/sections/IntroSection.svelte
@@ -2,7 +2,6 @@
 	import PersonalLink from '$components/PersonalLink.svelte';
 	import avatar from '$lib/assets/avatar.jpg'
 	import { Facebook, Github, Mail } from 'lucide-svelte'
-	import { fly } from 'svelte/transition';
 
 	interface IntroSectionProps {
 		isFirstVisit: boolean
@@ -48,10 +47,10 @@
 	})
 </script>
 
-<section class="w-full flex">
-	<div class="w-1/2 flex justify-center">
-		<!-- Banner profile -->
-		<div class="relative h-banner aspect-banner bg-banner bg-cover" class:animate-down={isFirstVisit}>
+<section class="w-full flex max-lg:flex-col max-lg:items-center max-lg:space-y-6">
+	<!-- Banner profile -->
+	<div class="w-1/2 max-2xl:w-1/3 flex justify-center">
+		<div class="relative h-banner max-lg:h-banner-lg aspect-banner bg-banner bg-cover" class:animate-down={isFirstVisit}>
 			<!-- Avatar -->
 			<div class="w-full h-[55%] flex justify-center items-end">
 				<div class="w-[83%] aspect-square relative">
@@ -65,22 +64,24 @@
 			</div>
 		</div>
 	</div>
-	<div class="w-1/3 text-yellow flex flex-col justify-center space-y-7">
-		<!-- Introduction -->
-		<h1 class="text-6xl font-semibold">Hi, It's <span>Binh</span></h1>
-		<h3 class="text-4xl font-semibold">
-			I'm a 
-			{#each descriptions as description, index}
-				{#if index == currentIndexDescription}
-					<span class="after:border-l-2 after:animate-cursor" in:typingAnimation={{speed: 1}}>{description}</span>
-				{/if}
-			{/each}
-		</h3>
-		<p class="text-lg">A skilled summoner in the realm of web development, wielding modern technologies to forge powerful and elegant digital experiences.</p>
-		<div class="flex space-x-6">
-			<PersonalLink href="https://github.com/SteGG200" Icon={Github}/>
-			<PersonalLink href="https://www.facebook.com/geor.steven/" Icon={Facebook}/>
-			<PersonalLink href="mailto:binhbhgl5@gmail.com" Icon={Mail}/>
+	<!-- Introduction -->
+	<div class="w-1/2 max-2xl:w-2/3 max-lg:w-full flex max-2xl:justify-center items-center">
+		<div class="w-2/3 max-lg:w-3/4 text-yellow flex flex-col justify-center space-y-7">
+			<h1 class="text-6xl max-lg:text-5xl font-semibold">Hi, It's <span>Binh</span></h1>
+			<h3 class="text-4xl max-lg:text-3xl font-semibold">
+				I'm a 
+				{#each descriptions as description, index}
+					{#if index == currentIndexDescription}
+						<span class="after:border-l-2 after:animate-cursor" in:typingAnimation={{speed: 1}}>{description}</span>
+					{/if}
+				{/each}
+			</h3>
+			<p class="text-lg">A skilled summoner in the realm of web development, wielding modern technologies to forge powerful and elegant digital experiences.</p>
+			<div class="flex space-x-6">
+				<PersonalLink href="https://github.com/SteGG200" Icon={Github}/>
+				<PersonalLink href="https://www.facebook.com/geor.steven/" Icon={Facebook}/>
+				<PersonalLink href="mailto:binhbhgl5@gmail.com" Icon={Mail}/>
+			</div>
 		</div>
 	</div>
 </section>
