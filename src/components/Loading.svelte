a0c35013b31fde9172298b318ed8f8ac83d08387
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-25 13:08:42 +0700

    Create loading screen

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -1,3 +1,9 @@
 <script lang="ts">
+	import { LoaderCircle } from "lucide-svelte";
+
 	
 </script>
+
+<div class="min-h-dvh flex justify-center items-center">
+	<LoaderCircle strokeWidth={1.5} size={80} class="text-yellow animate-spin" />
+</div>
