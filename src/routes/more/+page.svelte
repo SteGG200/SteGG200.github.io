bc0313b0b703973fb81f477e57de08763ac9a3ed
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-25 13:03:00 +0700

    Create more page

---
+++ src/routes/more/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Footer from "$components/footer/Footer.svelte";
+import Header from "$components/header/Header.svelte";
+
+	
+</script>
+
+<div class="min-h-dvh flex flex-col">
+	<Header/>
+	<main class="flex-1 text-yellow">
+		<p class="text-3xl text-center pt-6">
+			Sorry! I have not done this page yet.
+		</p>
+	</main>
+	<Footer/>
+</div>
