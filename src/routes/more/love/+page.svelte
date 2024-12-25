f06cbeea94babd0ba056b0401643c602bb06c215
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-25 15:13:11 +0700

    Add 01

---
+++ src/routes/more/love/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<main class="w-full min-h-dvh flex p-16 text-yellow text-xl">
+	<div class="flex-1 bg-dark-sm rounded-xl p-8">
+		<ul>
+			<li>
+				<a class="underline-decoration" href="/more/love/bach_nguyet_quang">01</a>
+			</li>
+			<li>
+				<a class="underline-decoration" href="/more/love/crush">02</a>
+			</li>
+		</ul>
+	</div>
+</main>
---
+++ src/routes/more/love/bach_nguyet_quang/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import avatar from '$lib/assets/more/01/02.jpg'
+	import _03 from '$lib/assets/more/01/03.mp4'
+	import _04 from '$lib/assets/more/01/04.mp4'
+	import _05 from '$lib/assets/more/01/05.mp4'
+	import _06 from '$lib/assets/more/01/06.mp4'
+	import _07 from '$lib/assets/more/01/07.jpg'
+	import _08 from '$lib/assets/more/01/08.jpg'
+
+	const info = [
+		{label: 'Ngày sinh', value: '07/04/2006'},
+		{label: 'Nơi sống', value: 'Chiết Giang, Trung Quốc'},
+		{label: 'Trường cấp 3', value: "Zhejiang Rui'an No.4 Middle School"},
+		{label: 'Đại học', value: 'Học viện Hý kịch Trung ương'},
+		{label: 'Chiều cao', value: '1m62-64'}
+	]
+</script>
+
+<main class="w-4xl mx-auto pt-20 text-yellow">
+	<div class="bg-dark-sm border-1 border-yellow rounded-xl p-6 space-y-4">
+		<img class="w-[220px] aspect-square border-1 border-yellow rounded-full" src={avatar} alt="Li Min Xuan">
+		<h2 class="text-2xl font-semibold">Lý Mẫn Hiên <span class="font-medium">(李闽轩)</span></h2>
+		<table>
+			<tbody>
+				{#each info as row}
+					<tr>
+						<td class="p-3 align-middle font-semibold">{row.label}:</td>
+						<td class="p-3 align-middle text-yellow-light">{row.value}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<p class="text-yellow-light"><span class="text-yellow font-semibold">Mô tả: </span>
+			Chị xinh và cute, đơn giản thế thôi 🥰 🥰. Mình biết việc chị làm nhưng mà mình lỡ mê vẻ đẹp này mất rồi 🥲 nên là mình vẫn có thể chấp nhận.
+		</p>
+		<p class="font-semibold text-lg">Dưới đây là một vài bức ảnh hoặc video mà mình có được:</p>
+		<div class="max-w-[400px] mx-auto space-y-2">
+			<video class="w-full h-auto" controls>
+				<track kind="captions"/>
+				<source src={_03} type="video/mp4">
+			</video>
+			<video class="w-full h-auto" controls>
+				<track kind="captions"/>
+				<source src={_04} type="video/mp4">
+			</video>
+			<video class="w-full h-auto" controls>
+				<track kind="captions"/>
+				<source src={_05} type="video/mp4">
+			</video>
+			<video class="w-full h-auto" controls>
+				<track kind="captions"/>
+				<source src={_06} type="video/mp4">
+			</video>
+			<img src={_07} alt="Li Min Xuan">
+			<img src={_08} alt="Li Min Xuan">
+		</div>
+		<p>Còn nhiều ảnh với video nữa nhưng mà không up lên được 🥲</p>
+	</div>
+</main>
