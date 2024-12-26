512f9765056380a821175cab3b98f39eb174515d
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-12-26 10:32:01 +0700

    Config more page

--- src/routes/more/+page.svelte
+++ src/routes/more/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import Footer from '$components/footer/Footer.svelte';
 	import Header from '$components/header/Header.svelte';
+
+	let position = {
+		top: Math.random() * 101,
+		left: Math.random() * 101
+	}
 </script>
 
 <div class="flex min-h-dvh flex-col">
 	<Header />
-	<main class="text-yellow flex-1">
+	<main class="relative text-yellow flex-1">
 		<p class="pt-6 text-center text-3xl">Sorry! I have not done this page yet.</p>
-		<a class="text-dark-sm" href="/more/love"> love </a>
+		<a class="absolute text-dark" style={`top: ${position.top}%; left: ${position.left}%;`} href="/more/love"> love </a>
 	</main>
 	<Footer />
 </div>
