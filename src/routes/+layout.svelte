23045e746c9a55cf707a9623b70689b0332e4af8
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-11-16 20:24:13 +0700

    refactor: format project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,9 @@
 
 <svelte:head>
 	<title>SteGG</title>
-	<meta name="description" content="A personal website of Nguyen Hoa Binh (SteGG)">
-	<meta name="keywords" content="personal website, stegg, Nguyen Hoa Binh, stegg200">
-	<meta name="author" content="SteGG">
+	<meta name="description" content="A personal website of Nguyen Hoa Binh (SteGG)" />
+	<meta name="keywords" content="personal website, stegg, Nguyen Hoa Binh, stegg200" />
+	<meta name="author" content="SteGG" />
 </svelte:head>
 
 {@render children()}
--- src/routes/more/+page.svelte
+++ src/routes/more/+page.svelte
@@ -5,14 +5,20 @@
 	let position = {
 		top: Math.random() * 101,
 		left: Math.random() * 101
-	}
+	};
 </script>
 
 <div class="flex min-h-dvh flex-col">
 	<Header />
-	<main class="relative text-yellow flex-1">
+	<main class="text-yellow relative flex-1">
 		<p class="pt-6 text-center text-3xl">Sorry! I have not done this page yet.</p>
-		<a class="absolute text-dark" style={`top: ${position.top}%; left: ${position.left}%;`} href="/more/love"> love </a>
+		<a
+			class="text-dark absolute"
+			style={`top: ${position.top}%; left: ${position.left}%;`}
+			href="/more/love"
+		>
+			love
+		</a>
 	</main>
 	<Footer />
 </div>
