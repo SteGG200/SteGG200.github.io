38d2fcae5aa05f389919b7c30b3881699f274654
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-11-16 15:57:38 +0700

    feat: display animation of banner profile regardless of that isn't the first time user visit website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,25 @@
 <script lang="ts">
 	import Footer from '$components/footer/Footer.svelte';
 	import Header from '$components/header/Header.svelte';
-	import Loading from '$components/Loading.svelte';
 	import ExperiencesSection from '$components/sections/ExperiencesSection.svelte';
 	import InformationSection from '$components/sections/InformationSection.svelte';
 	import IntroSection from '$components/sections/IntroSection.svelte';
 	import ProjectsSection from '$components/sections/ProjectsSection.svelte';
 	import { ChevronsDown } from 'lucide-svelte';
-
-	let isLoading = $state(true);
-	let isFirstVisit = $state(true);
-
-	$effect(() => {
-		// Check if user first visit the website
-		const firstVisitValue = sessionStorage.getItem('isFirstVisit');
-		if (!firstVisitValue) {
-			sessionStorage.setItem('isFirstVisit', '1');
-		}
-		isFirstVisit = firstVisitValue ? false : true;
-
-		isLoading = false;
-	});
 </script>
 
 <Header />
 
-{#if isLoading}
-	<Loading />
-{:else}
-	<main class="mb-20 flex flex-col space-y-20">
-		<div class="h-full">
-			<IntroSection {isFirstVisit} />
-			<div class="mt-16 mb-10 w-full">
-				<ChevronsDown size={32} class="text-yellow mx-auto animate-bounce" />
-			</div>
+<main class="mb-20 flex flex-col space-y-20">
+	<div class="h-full">
+		<IntroSection />
+		<div class="mt-16 mb-10 w-full">
+			<ChevronsDown size={32} class="text-yellow mx-auto animate-bounce" />
 		</div>
-		<InformationSection />
-		<ExperiencesSection />
-		<ProjectsSection />
-	</main>
-{/if}
+	</div>
+	<InformationSection />
+	<ExperiencesSection />
+	<ProjectsSection />
+</main>
 
 <Footer />
