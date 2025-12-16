de3c70e5170deda31f55bcdf303c2355212e35da
Nguyễn Hòa Bình <steven.nguyenhoabinh@gmail.com>
2025-12-16 08:42:01 +0700

    fix: add gap between blogcard in blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -15,7 +15,7 @@
 
 <main class="min-h-dvh w-full space-y-8 py-12">
 	<h1 class="text-center text-4xl font-semibold max-md:text-3xl">SteGG's Blogs</h1>
-	<div class="w-blogs-list mx-auto max-xl:w-5/6 max-md:w-11/12">
+	<div class="w-blogs-list mx-auto max-xl:w-5/6 max-md:w-11/12 space-y-4">
 		{#each data.blogs as blog, index (index)}
 			<BlogCard id={index + 1} title={blog.title} tags={blog.tags} createdAt={blog.createdAt} />
 		{/each}
